eb031f20f18dab78acd061f61e885d361e9f6ef7
JiaJingZ <jiajingz396@gmail.com>
2023-01-23 19:09:49 +0800

    查询单条或多条动态

--- src/controller/moment.controller.js
+++ src/controller/moment.controller.js
@@ -8,6 +8,18 @@ class MomentController {
     console.log(result)
     ctx.body = result
   }
+
+  async detail(ctx, next) {
+    const { momentID } = ctx.params
+    const result = await service.getMomentById(momentID)
+    ctx.body = result
+  }
+
+  async list(ctx, next) {
+    const { offset, size } = ctx.query
+    const result = await service.getMomentList({offset, size})
+    ctx.body = result
+  }
 }
 
 module.exports = new MomentController()
--- src/router/moment.router.js
+++ src/router/moment.router.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router')
 const { verifyauth } = require('../middleware/user.middleware')
-const { create } = require('../controller/moment.controller')
+const { create, detail, list } = require('../controller/moment.controller')
 
 const momentRouter = new Router({prefix: '/moment'})
 
 momentRouter.post('/create', verifyauth, create)
+momentRouter.get('/detail/:momentID', detail)
+momentRouter.get('/list', list)
 
 module.exports = momentRouter
